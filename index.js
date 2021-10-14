a098a93568dab971a4f3c1efeb23fbb8e4a725c7
unknown <martinprlt02@gmail.com>
2021-10-14 20:46:30 -0300

    martinprlt

---
+++ index.js
@@ -0,0 +1,12 @@
+//definimos una variable para mi nombre
+const firstName = "Martin"
+//definimos una variable para mi apellido
+const lastname = "Peralta"
+// y definimos una variable para mi edad
+var age = 18
+// ahora concatenamos las variables e imprimimos en la consola
+ console.log(firstName + " " + lastname + " tiene " + age + " años"); 
+// sumamos un numero a mi edad
+ age= age+1
+ // e imprimimos nuevamente 
+ console.log(firstName + " " + lastname + " tiene " + age + " años");
